6f46afcb7aa72eb7dff78641cb14e14b5f349753
Ric_j <ricjohn62@gmail.com>
2024-01-22 23:18:20 +0800

    progress

---
+++ GROUP 5 Astral/Assets/Mainstart.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainstart : MonoBehaviour
+{
+
+    public void quit(){
+
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
