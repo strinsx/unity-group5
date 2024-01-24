f5144b0dc7616bfae4e7cb54d4596c5c82847a27
Ric_j <ricjohn62@gmail.com>
2024-01-24 13:42:21 +0800

    progress

---
+++ GROUP 5 Astral/Assets/Scripts/rotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public float rotationZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.Rotate(Vector3.forward * rotationZ * Time.deltaTime);
+    }
+}
